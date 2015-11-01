e76f549a62c67b78f3dbeadbf7321db675ab46df
lemastero <adres.mistrza@gmail.com>
2015-11-01 20:51:09 +0100

    Add english dictionary and tokenize multiple words

--- src/main/scala/castspell/nlp/Tokenizer.scala
+++ src/main/scala/castspell/nlp/Tokenizer.scala
@@ -1,7 +1,39 @@
 package castspell.nlp
 
+import scala.io.Source
+
 object Tokenizer {
 
-  def tokenize(input: String) = List(input)
+  var wordsList = List[String]()
+
+  def initialize() = {
+    val source = Source.fromURL(getClass.getResource("/lowercase_english.txt"))
+    wordsList = try source.getLines.toList finally source.close
+    Tokenizer
+  }
+
+  def initialize(list: List[String]) = {
+    wordsList = list
+    Tokenizer
+  }
+
+  def tokenize(input: String): List[String] = {
+    def tokenize(remainingInput: String, tokens:List[String]): List[String] = {
+      if (remainingInput == "") tokens
+      else {
+        val potentialTokens = wordsList.filter(remainingInput.startsWith)
+          if(potentialTokens.isEmpty) throw new NoEnoughWordsToParse(s"Unable to parse ${remainingInput}")
+        else {
+            val nextToken =  potentialTokens.maxBy(_.length)
+            tokenize(removePrefix(remainingInput, nextToken), nextToken :: tokens)
+          }
+      }
+    }
+
+    tokenize(input.toLowerCase, List[String]()).reverse
+  }
+
+  private[nlp] def removePrefix(input: String, prefix: String) =
+    input.substring(prefix.length)
 
 }
--- src/test/scala/castspell/nlp/TokenizerSpec.scala
+++ src/test/scala/castspell/nlp/TokenizerSpec.scala
@@ -4,9 +4,34 @@ import org.scalatest.{MustMatchers, FunSpec}
 
 class TokenizerSpec extends FunSpec with MustMatchers {
 
-  describe("Tokenizer") {
+  val tokenizer = Tokenizer.initialize(List("quick", "dog", "cat",
+    "jump", "over", "fox", "blue", "the", "brown", "lazy"))
+
+  describe("Tokenizer removePrefix") {
+    it("replace first character") {
+      Tokenizer.removePrefix("the", "t") mustBe "he"
+    }
+
+    it("replace full word") {
+      Tokenizer.removePrefix("cat", "cat") mustBe ""
+    }
+  }
+
+  describe("Tokenizer tokenize") {
     it("recognize simple word") {
-      Tokenizer.tokenize("the") mustBe List("the")
+      tokenizer.tokenize("the") mustBe List("the")
+    }
+
+    it("recognize words") {
+      tokenizer.tokenize("Thequickbrownfoxjumpoverlazydog") mustBe List(
+        "the", "quick", "brown", "fox",
+        "jump", "over", "lazy", "dog")
+    }
+
+    withClue("recognize words") {
+      intercept[NoEnoughWordsToParse] {
+        tokenizer.tokenize("bigos")
+      }
     }
   }
 
